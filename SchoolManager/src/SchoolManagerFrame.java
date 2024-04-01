b615bedceb578615b1b2ec53a1c32c0fdb602681
k1641604 <160759289+k1641604@users.noreply.github.com>
2024-04-01 14:07:53 -0500

    Update SchoolManagerFrame.java

--- SchoolManager/src/SchoolManagerFrame.java
+++ SchoolManager/src/SchoolManagerFrame.java
@@ -1,2 +1,45 @@
-public class SchoolManagerFrame {
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.ListSelectionEvent;
+
+public class SchoolManagerFrame extends JFrame{
+
+    JMenuBar menubar = new JMenuBar();
+    JTable teachersList = new JTable();
+    JTable courseList = new JTable();
+    JTable sectionList = new JTable();
+    JTable enrollmentList = new JTable();
+    JTable studentList = new JTable();
+
+    JButton deleteTables = new JButton();
+
+
+    public SchoolManagerFrame(){
+        super("School Manager");
+        setSize(800, 700);
+        setLayout(null);
+
+        teachersList.setBounds(20, 20, 148, 660);
+        add(teachersList);
+
+        courseList.setBounds(178, 20, 148, 660);
+        add(courseList);
+
+        sectionList.setBounds(336, 20, 148, 660);
+        add(sectionList);
+
+        enrollmentList.setBounds(494, 20, 148, 660);
+        add(enrollmentList);
+
+        studentList.setBounds(651, 20, 148, 660);
+        add(studentList);
+
+        //deleteTables.setBounds();
+
+        setVisible(true);
+    }
+
+
 }
